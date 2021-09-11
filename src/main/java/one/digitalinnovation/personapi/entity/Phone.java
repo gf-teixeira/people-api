71ae1806ed3c59416738c975515b43cd2caaa1e5
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-09-11 17:53:04 -0300

    Developed ´create person´ operation

--- src/main/java/one/digitalinnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinnovation.personapi.enums.PhoneType;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
 
