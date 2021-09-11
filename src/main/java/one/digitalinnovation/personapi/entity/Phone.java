0618255243f1f8d39c1d7ffa7d9410d321014fdd
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-09-11 16:53:04 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalinnovation.personapi.enums.PhoneType;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+
+@Entity
+@Data //insere getters e setters
+@Builder //padrão de projetos
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //IDENTITY = incrementa
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
